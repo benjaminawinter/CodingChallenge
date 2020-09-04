4a5e4ce4361def4d394ea49054439dbf4873950c
Ben Winter <benjamin.a.winter@gmail.com>
2020-09-04 01:46:47 -0500

    implement breadth-first search

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace CodingChallenge.FamilyTree
 {
@@ -6,7 +6,35 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            var peopleQueue = new Queue<Person>();
+            peopleQueue.Enqueue(person);
+
+            var people = new List<Person>
+            {
+                person
+            };
+
+            while (peopleQueue.Count > 0)
+            {
+                var e = peopleQueue.Dequeue();
+                if (e.Name == descendantName)
+                {
+                    return e.Birthday.ToString("MMMM");
+                }
+
+                foreach (var descendant in e.Descendants)
+                {
+                    if (people.Contains(descendant))
+                    {
+                        continue;
+                    }
+
+                    peopleQueue.Enqueue(descendant);
+                    people.Add(descendant);
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
